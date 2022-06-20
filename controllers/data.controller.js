7d452cd4e22f4f1fe7a6956d3373ca4bffce56e8
N-pacis <pacisnkubito@gmail.com>
2022-06-20 10:25:59 +0200

    Update data.controller.js

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -6,7 +6,7 @@ export const postData = async (req, res) => {
             dt = JSON.stringify(dt)
             data.push(dt.replace(/[{}]/g,'').replace(/[""]/g,''));
         })
-        console.warn("Uploaded successfully")
+        console.warn({message: "Uploaded successfully",data,status:200})
         return res.json({message: "Uploaded successfully",data,status:200})
     } catch (ex) {
         res.status(400).send(ex.message)
